eb30962e48f12943accf777c008edd0eba759fe6
Md.Mahidur Rahman <mahidurrahmanmahi865125@gmail.com>
2023-08-21 22:23:49 +0600

    design part done

---
+++ JS/card.js
@@ -0,0 +1,15 @@
+function handleClikbtn(target){
+    console.log(target.parentNode.childNodes[1].innerText);
+    
+}
+
+// document.getElementById('card-btn').addEventListener('click',function(){
+//     const getName =document.getElementById('getName');
+//     const catchName = getName.innerText;
+    
+//     const purchaseName = document.getElementById('purchaseName');
+//     const catchPurchaseName = purchaseName.innerText;
+    
+//     purchaseName.innerText = catchName;
+
+// })
